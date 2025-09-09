d3ed73965510043cad5535eb3cebf8dcfa848d93
chriseze212 <chriseze212@gmail.com>
2025-09-09 12:37:11 +0000

    added files

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+additionOfNumber = number1 + number2
+mulOfNumber = number1 * number2
+subOfNumber = number1 - number2
+
+print(f"Addition of {number1} and {number2} is {additionOfNumber}")
+print(f"Subtraction of {number1} and {number2} is {subOfNumber}")
+print(f"Multiplication of {number1} and {number2} is {mulOfNumber}")
